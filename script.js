1239dc58156a8c792fd5b526d42106a710bffa42
Ben H <ben.helfstein@icloud.com>
2026-04-09 11:43:59 -0400

    awkdsxdn  hoa  njsh ihfdd gelkafh kjfh aef iuwugr ihheiur 3uwhr ew hr iues goiw gy

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+console.log("VavaScript");
+// This is a comment
+/* This is a 
+multi-line comment */
+
+//variables are named storage containers for data
+let username;
+username = "Ben";
+
+let pokemon = "dragapult"
+
+console.log(username);
+
+let luckNum = 13;
