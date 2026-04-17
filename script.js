3cd43534052938c774404c4f2fd0a75bccd326e7
Ben H <ben.helfstein@icloud.com>
2026-04-17 12:44:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,34 @@ let pokemon = "dragapult"
 console.log(username);
 
 let luckNum = 13;
+
+console.log(luckNum);
+
+luckNum = 4;
+
+console.log(luckNum);
+
+//math operators (+,-,*,/,**,%)
+let age = 17;
+let ageInTen = age + 10;
+let ageDogYears = age / 7;
+
+console.log(ageDogYears);
+
+//pemdas
+let score = 0;
+funNumber = (score + 929 - 39) * 67 / 932**2;
+console.log(score);
+
+//concatanate
+let firstName = "Ben";
+let lastName = "H";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+console.log("My fun number is: " + funNumber);
+let user = "Ms. Walyer"
+let message = "walcome " + user + "!";
+console.log(message);
