5d0cb8873b6300828192428033c02202a98de55e
Ben H <ben.helfstein@icloud.com>
2026-04-28 09:42:21 -0400

    slakc
    askc aich ajcna j  sjan ajsbsjb aja ais aj

--- script.js
+++ script.js
@@ -59,3 +59,35 @@ function potionRecipie () {
 
 //call function
 potionRecipie();
+
+
+//making booleans
+
+console.log(25 > 4);
+console.log("is 10 less than 2");
+console.log(10 < 2);
+
+//check equality using the == operator
+console.log(1 == 1);
+console.log(1 != 1);
+
+//strings work too
+console.log("speggeti" == "speggeti");
+console.log("m" < "a");
+console.log("M" < "a");
+
+let password = "Pasword67";
+console.log(password == "Password67");
+
+
+/* if statement use a condition/boolen expresion to choose which cide to run*/
+
+let myAge = 18;
+
+if (myAge >= 18) {
+    console.log("you can get your NY licence");
+} else if (myAge >= 17 && takenDriversEd == true) {
+    console.log("you can get your NY licence");
+} else {
+    console.log("outa luck");
+}
